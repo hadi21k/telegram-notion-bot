9a805d6a2229ddf580924aa43843ce1259e0b11b
hadi21k <hadidyeb@gmail.com>
2023-10-03 19:56:50 +0300

    remove bot on polling error

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const connectToDB = require("./utils/connectToDB");
 const dotenv = require("dotenv");
 const logger = require("./middlewares/logger");
-const bot = require("./utils/bot");
 
 dotenv.config();
 
@@ -14,19 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-bot.setMyCommands([
-  { command: "start", description: "Start the bot" },
-  { command: "connect", description: "Connect your Notion account" },
-  {
-    command: "create",
-    description: "Send a Notion page URL to create a database",
-  },
-  {
-    command: "add",
-    description: "Add a new link to the database",
-  },
-]);
-
 app.post("/new-message", newMessageController);
 
 // bot.onText(/\/start/, start);
@@ -35,8 +21,6 @@ app.post("/new-message", newMessageController);
 // bot.onText(/\/add (\S+) (.+) (.+)/, addToDatabase);
 
 app.use("/api/auth", authRouter);
-
-bot.on("polling_error", (error) => logger.error(error));
 
 app.listen(process.env.PORT, async () => {
   await connectToDB();
